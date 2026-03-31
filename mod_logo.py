bd158ebaaa1db054804272193322c7536b7c1241
Robertsabin <sabinesteban@gmail.com>
2026-03-31 18:21:08 +0200

    Create mod_logo.py

---
+++ mod_logo.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def mostrar_logo(path=""):
+    # Como no tenemos el archivo físico del logo todavía, 
+    # ponemos un texto o lo dejamos pasar para que no dé error.
+    st.write("🏢 **Sistema de Inventario Industrial**")
